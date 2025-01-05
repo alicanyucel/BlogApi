eec014cef6ea938ef94af6b7e00d35a53cd541b7
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-05 10:08:25 +0300

    swagger üzerinden ayağa kaldırma https://localhost:7272/index.html

--- BlogApi/Program.cs
+++ BlogApi/Program.cs
@@ -1,4 +1,3 @@
-
 namespace BlogApi
 {
     public class Program
@@ -7,27 +6,35 @@ namespace BlogApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+           
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+          
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
+            // swagger üzerinden ayaða kaldýrma https://localhost:7272/index.html
+      
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+
+                   
+                    c.RoutePrefix = string.Empty;  
+                });
             }
 
+      
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
-
+           
             app.MapControllers();
 
+          
             app.Run();
         }
     }
